8589523fa44e536f6aa138ec03094a7bde5e3075
hemanth <hemanth111sankar@gmail.com>
2021-05-30 10:31:08 +0530

    SUBMISSION OF HOMEWORK

--- estimate.py
+++ estimate.py
@@ -1,7 +1,20 @@
+import random
 def wallis(x):
   p=1
   for i in range(1,x):
     p*= (4*(i**2)/((4*(i**2))-1))
   return (2*p)
+  
+def monte_carlo(n):
+  c=0
+  s=0
+  for i in range(n):
+    x=random.random()
+    y=random.random()
+    if((x**2)+(y**2)<1):
+      c+=1
+    else:
+      s+=1
+  return (4*(c/(c+s)))
   
 
---
+++ file.py
@@ -0,0 +1,3 @@
+f=open("README")
+r=f.read(2)
+print(r)
