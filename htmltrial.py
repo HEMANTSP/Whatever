c4beac7ccaa7a66970b35b6f523dea1a27d771b3
hemanth <hemanth111sankar@gmail.com>
2021-06-18 19:45:02 +0530

    Blocks

--- htmltrial.py
+++ htmltrial.py
@@ -1,7 +1,11 @@
 import psycopg2
 from flask import Flask,request,render_template
+import jinja2
 
 app=Flask("new",template_folder='templates')
+
+dc=psycopg2.connect("dbname=heros")
+c=dc.cursor()
 
 @app.route("/")
 def home():
@@ -9,19 +13,17 @@ def home():
 
 @app.route("/next")
 def next():
-  dc=psycopg2.connect("dbname=heros")
-  c=dc.cursor()
-  d=c.execute("select name ,gender from heros")
-  data=c.fetchall()
-  '''
-  l=[]
-  for a,b in c.fetchall():
+    d=c.execute("select name ,gender from heros")
+    data=c.fetchall()
+    '''
+    l=[]
+    for a,b in c.fetchall():
     x= f" NAME :: {a}  GENDER :: {b}"
     l.append(x)
-  l="\n".join(l)
-  return (render_template("nextpage.html", r=l))
- '''
-  return (render_template("nextpage.html",data=data))
+    l="\n".join(l)
+    return (render_template("nextpage.html", r=l))
+    '''
+    return (render_template("nextpage.html",data=data))
  
 def main():
   app.run(debug=True)
